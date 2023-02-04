0fb85c019b64a2a3fa4a5fddfe65b9d8feccb0a5
Faruk CANSIZ <94459959+FarukCNSZ@users.noreply.github.com>
2023-02-05 01:24:23 +0300

    First commit

---
+++ newsApp/Model/News.swift
@@ -0,0 +1,8 @@
+//
+//  News.swift
+//  newsApp
+//
+//  Created by Faruk CANSIZ on 5.02.2023.
+//
+
+import Foundation
---
+++ newsApp/View/NewsCell.swift
@@ -0,0 +1,29 @@
+//
+//  NewsCell.swift
+//  newsApp
+//
+//  Created by Faruk CANSIZ on 5.02.2023.
+//
+
+import UIKit
+
+class NewsCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var storyLabel: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
