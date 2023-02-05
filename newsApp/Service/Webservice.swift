63ca6c857cb54822bf74185e78f93251ddfdd507
Faruk CANSIZ <94459959+FarukCNSZ@users.noreply.github.com>
2023-02-05 14:56:46 +0300

    Escaping closure

---
+++ newsApp/Service/Webservice.swift
@@ -0,0 +1,25 @@
+//
+//  Webservice.swift
+//  newsApp
+//
+//  Created by Faruk CANSIZ on 5.02.2023.
+//
+
+import Foundation
+
+class Webservice {
+    
+    
+    //Bir fonksiyon içerisinde closure'ı parametre olarak vericeksek ve fonksiyonun işi bittikten sonra döndürülcek bir tamamlama bloğu yapıyorsak escaping kullanılır.
+    func haberleriIndir(url: URL, completion: @escaping ([News?]) -> () ) {
+        
+        
+        
+        
+    }
+    
+    
+}
+
+
+
