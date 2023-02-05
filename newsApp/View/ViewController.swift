f4dec8f4b39c3aa7a549a407ea2d8e05dda77bc7
Faruk CANSIZ <94459959+FarukCNSZ@users.noreply.github.com>
2023-02-05 19:15:16 +0300

    End of the app

--- newsApp/View/ViewController.swift
+++ newsApp/View/ViewController.swift
@@ -19,6 +19,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.delegate = self
         tableView.dataSource = self
         
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = UITableView.automaticDimension
+        
         veriAl()
         
         }
@@ -50,6 +53,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         cell.storyLabel.text = newsViewModel.story
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
 
 }
 
