530f7a10a987b3d67d756a8f79b05fce871300d1
Faruk CANSIZ <94459959+FarukCNSZ@users.noreply.github.com>
2023-02-05 16:52:52 +0300

    Download data

--- newsApp/Service/Webservice.swift
+++ newsApp/Service/Webservice.swift
@@ -13,6 +13,22 @@ class Webservice {
     //Bir fonksiyon içerisinde closure'ı parametre olarak vericeksek ve fonksiyonun işi bittikten sonra döndürülcek bir tamamlama bloğu yapıyorsak escaping kullanılır.
     func haberleriIndir(url: URL, completion: @escaping ([News?]) -> () ) {
         
+        //internetten bir istek yapınca URLSession kullanılır
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(nil)
+            } else if let data = data {
+                let haberlerDizisi = try?  JSONDecoder().decode([News].self, from: data)
+                
+                if let haberlerDizisi = haberlerDizisi {
+                    completion(haberlerDizisi)
+                    
+                }
+                
+            }
+        }.resume()
+        //Bu istek bittikten sonra ne yapılacağını yazdığımız bir completion(tamamlama) bloğu var
         
         
         
